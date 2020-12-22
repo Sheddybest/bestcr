890a49c09f18274abf8d0b7b1a60e0d61b5c5bdc
Sheddybest <mr.juniorjayz@gmail.com>
2020-12-22 20:11:58 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package BestCR
+ */
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=divice-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0">
+    <meta http-equiv="X-UA-Compatable" content="ig-edge">
+    <title>WordPress Theme</title>
+</head>
+<body>
+Hello World
+</body>
+</html>
